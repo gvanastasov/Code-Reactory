39bcab9fe30f0a8ba451fe12a8f42008e7395979
gva <gvanastasov@abv.bg>
2018-01-20 16:31:34 +0100

    babelify vue

--- src/gulp/tasks/scripts.js
+++ src/gulp/tasks/scripts.js
@@ -2,10 +2,17 @@ const configPath = '../config.js';
 
 var config     = require(configPath),
     browserify = require('browserify'),
+    vueify     = require('vueify'),
+    babelify   = require('babelify'),
+    envify     = require('envify/custom'),
     source     = require('vinyl-source-stream');
 
 module.exports = function(gulp, plugins){
-    browserify(config.paths.js.src + 'app.js')
+    browserify({
+        global: true,
+        entries: config.paths.js.src + 'app.js',
+        transform: [vueify, babelify, envify({ NODE_ENV: 'production' })] 
+    })
     .bundle()
     .pipe(source('main.js'))
     .pipe(gulp.dest(config.paths.js.dist));
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
 var math = require('gl-matrix');
+import Vue from 'vue';
+
+var app = new Vue({
+    el: 'app',
+    mounted: function(){
+        console.log('vue testing');
+    }
+});
